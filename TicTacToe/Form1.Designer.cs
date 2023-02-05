cea7848a1393c04737f2a606603b3e07888f67fa
ukasz <lukaszkrz@protonmail.com>
2023-02-05 13:12:29 +0100

    Added Player vs Computer mode

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pvPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vsComputerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -72,15 +74,31 @@
             // 
             // newGameToolStripMenuItem
             // 
+            this.newGameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pvPToolStripMenuItem,
+            this.vsComputerToolStripMenuItem});
             this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
-            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
             this.newGameToolStripMenuItem.Text = "New game";
-            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // pvPToolStripMenuItem
+            // 
+            this.pvPToolStripMenuItem.Name = "pvPToolStripMenuItem";
+            this.pvPToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.pvPToolStripMenuItem.Text = "vs Person";
+            this.pvPToolStripMenuItem.Click += new System.EventHandler(this.pvPToolStripMenuItem_Click);
+            // 
+            // vsComputerToolStripMenuItem
+            // 
+            this.vsComputerToolStripMenuItem.Name = "vsComputerToolStripMenuItem";
+            this.vsComputerToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.vsComputerToolStripMenuItem.Text = "vs Computer";
+            this.vsComputerToolStripMenuItem.Click += new System.EventHandler(this.vsComputerToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -95,7 +113,7 @@
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
             this.aboutToolStripMenuItem.Text = "About";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
@@ -103,100 +121,118 @@
             // 
             this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.A1.Location = new System.Drawing.Point(16, 45);
-            this.A1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.A1.Margin = new System.Windows.Forms.Padding(2);
             this.A1.Name = "A1";
             this.A1.Size = new System.Drawing.Size(75, 81);
             this.A1.TabIndex = 1;
             this.A1.UseVisualStyleBackColor = true;
             this.A1.Click += new System.EventHandler(this.buttonClick);
+            this.A1.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.A1.MouseLeave += new System.EventHandler(this.MouseLeave);
             // 
             // A2
             // 
             this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.A2.Location = new System.Drawing.Point(96, 45);
-            this.A2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.A2.Margin = new System.Windows.Forms.Padding(2);
             this.A2.Name = "A2";
             this.A2.Size = new System.Drawing.Size(75, 81);
             this.A2.TabIndex = 2;
             this.A2.UseVisualStyleBackColor = true;
             this.A2.Click += new System.EventHandler(this.buttonClick);
+            this.A2.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.A2.MouseLeave += new System.EventHandler(this.MouseLeave);
             // 
             // A3
             // 
             this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.A3.Location = new System.Drawing.Point(176, 45);
-            this.A3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.A3.Margin = new System.Windows.Forms.Padding(2);
             this.A3.Name = "A3";
             this.A3.Size = new System.Drawing.Size(75, 81);
             this.A3.TabIndex = 3;
             this.A3.UseVisualStyleBackColor = true;
             this.A3.Click += new System.EventHandler(this.buttonClick);
+            this.A3.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.A3.MouseLeave += new System.EventHandler(this.MouseLeave);
             // 
             // B1
             // 
             this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.B1.Location = new System.Drawing.Point(16, 131);
-            this.B1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.B1.Margin = new System.Windows.Forms.Padding(2);
             this.B1.Name = "B1";
             this.B1.Size = new System.Drawing.Size(75, 81);
             this.B1.TabIndex = 4;
             this.B1.UseVisualStyleBackColor = true;
             this.B1.Click += new System.EventHandler(this.buttonClick);
+            this.B1.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.B1.MouseLeave += new System.EventHandler(this.MouseLeave);
             // 
             // B2
             // 
             this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.B2.Location = new System.Drawing.Point(96, 131);
-            this.B2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.B2.Margin = new System.Windows.Forms.Padding(2);
             this.B2.Name = "B2";
             this.B2.Size = new System.Drawing.Size(75, 81);
             this.B2.TabIndex = 5;
             this.B2.UseVisualStyleBackColor = true;
             this.B2.Click += new System.EventHandler(this.buttonClick);
+            this.B2.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.B2.MouseLeave += new System.EventHandler(this.MouseLeave);
             // 
             // B3
             // 
             this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.B3.Location = new System.Drawing.Point(176, 131);
-            this.B3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.B3.Margin = new System.Windows.Forms.Padding(2);
             this.B3.Name = "B3";
             this.B3.Size = new System.Drawing.Size(75, 81);
             this.B3.TabIndex = 6;
             this.B3.UseVisualStyleBackColor = true;
             this.B3.Click += new System.EventHandler(this.buttonClick);
+            this.B3.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.B3.MouseLeave += new System.EventHandler(this.MouseLeave);
             // 
             // C1
             // 
             this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.C1.Location = new System.Drawing.Point(16, 217);
-            this.C1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.C1.Margin = new System.Windows.Forms.Padding(2);
             this.C1.Name = "C1";
             this.C1.Size = new System.Drawing.Size(75, 81);
             this.C1.TabIndex = 7;
             this.C1.UseVisualStyleBackColor = true;
             this.C1.Click += new System.EventHandler(this.buttonClick);
+            this.C1.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.C1.MouseLeave += new System.EventHandler(this.MouseLeave);
             // 
             // C2
             // 
             this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.C2.Location = new System.Drawing.Point(96, 217);
-            this.C2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.C2.Margin = new System.Windows.Forms.Padding(2);
             this.C2.Name = "C2";
             this.C2.Size = new System.Drawing.Size(75, 81);
             this.C2.TabIndex = 8;
             this.C2.UseVisualStyleBackColor = true;
             this.C2.Click += new System.EventHandler(this.buttonClick);
+            this.C2.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.C2.MouseLeave += new System.EventHandler(this.MouseLeave);
             // 
             // C3
             // 
             this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.C3.Location = new System.Drawing.Point(176, 217);
-            this.C3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.C3.Margin = new System.Windows.Forms.Padding(2);
             this.C3.Name = "C3";
             this.C3.Size = new System.Drawing.Size(75, 81);
             this.C3.TabIndex = 9;
             this.C3.UseVisualStyleBackColor = true;
             this.C3.Click += new System.EventHandler(this.buttonClick);
+            this.C3.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.C3.MouseLeave += new System.EventHandler(this.MouseLeave);
             // 
             // whosTurn
             // 
@@ -226,7 +262,7 @@
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "TicTacToe";
@@ -256,6 +292,8 @@
         private System.Windows.Forms.Button C2;
         private System.Windows.Forms.Button C3;
         private System.Windows.Forms.Label whosTurn;
+        private System.Windows.Forms.ToolStripMenuItem pvPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vsComputerToolStripMenuItem;
     }
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -13,6 +13,7 @@ namespace TicTacToe
     public partial class Form1 : Form
     {
         bool turn = false;//false = X; true = O
+        bool isPvc = false; //true = player vs computer
         int turnCount = 0;
 
         public Form1()
@@ -35,19 +36,39 @@ namespace TicTacToe
             Application.Exit();
         }
 
+        
+
         private void buttonClick(object sender, EventArgs e)
         {
             Button button = (Button)sender;
+            //if (turn & !isPvc)
+            //{
+            //    button.Text = "O";
+            //    whosTurn.Text = "X turn";
+            //}
+            //else if (turn & isPvc)
+            //{
+            //    computerMakeMove();
+            //    whosTurn.Text = "Computer's turn";
+            //}
+            //else
+            //{
+            //    button.Text = "X";
+            //    whosTurn.Text = "O turn";
+            //}
+
             if (turn)
             {
+                whosTurn.Text = "X turn!";
                 button.Text = "O";
-                whosTurn.Text = "X turn";
             }
             else
             {
+                whosTurn.Text = "O turn!";
                 button.Text = "X";
-                whosTurn.Text = "O turn";
             }
+            
+
 
             turn = !turn;
 
@@ -56,6 +77,38 @@ namespace TicTacToe
             turnCount++;
 
             checkForWinner();
+            if ((turn) && (isPvc))
+            {
+                computerMakeMove();
+            }
+        }
+
+        private void MouseEnter(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Enabled)
+            {
+                
+                if (turn && !isPvc)
+                {
+                    button.ForeColor = Color.Red;
+                    button.Text = "O";
+                }
+                else
+                {
+                    button.ForeColor = Color.Blue;
+                    button.Text = "X";
+                }
+            }
+        }
+
+        private void MouseLeave(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Enabled)
+            {
+                button.Text = "";
+            }
         }
 
         private void checkForWinner()
@@ -93,12 +146,14 @@ namespace TicTacToe
                     winner = "O";
 
                 MessageBox.Show(winner + " wins!", "Congratulation!");
+                restart();
             }
             else
             {
                 if(turnCount == 9)
                 {
                     MessageBox.Show("Draw!", "GG!");
+                    restart();
                 }
             }
         }
@@ -116,10 +171,121 @@ namespace TicTacToe
             catch { }
         }
 
-        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        private void computerMakeMove()
+        {
+            //1. win
+            //2. block
+            //3. corner
+            //4. open space
+
+            Button move = null;
+            //look for win
+            move = lookForWinOrBlock("O");      //look for win
+            if(move == null)
+            {
+                move = lookForWinOrBlock("X");  //look for block
+                if(move == null)
+                {
+                    move = lookForCorner();
+                    if(move == null)
+                    {
+                        move = lookForSpace();
+                    }
+                }
+            }
+            move.PerformClick();
+
+        }
+
+        private Button lookForCorner()
+        {
+            if (A1.Text == "") return A1;
+            if (A3.Text == "") return A3;
+            if (C1.Text == "") return C1;
+            if (C3.Text == "") return C3;
+            return null;
+
+        }
+
+        private Button lookForSpace()
+        {
+            Button b = null;
+            foreach(Control c in Controls)
+            {
+                b = c as Button;
+                if(b != null)
+                {
+                    if (b.Text == "") return b;
+                }
+            }
+            return null;
+        }
+
+        private Button lookForWinOrBlock(string v)
+        {
+            //Hor lines
+            if ((A1.Text == v) && (A2.Text == v) && (A3.Text == "")) return A3;
+            if ((A1.Text == v) && (A2.Text == "") && (A3.Text == v)) return A2;
+            if ((A1.Text == "") && (A2.Text == v) && (A3.Text == v)) return A1;
+
+            if ((B1.Text == v) && (B2.Text == v) && (B3.Text == "")) return B3;
+            if ((B1.Text == v) && (B2.Text == "") && (B3.Text == v)) return B2;
+            if ((B1.Text == "") && (B2.Text == v) && (B3.Text == v)) return B1;
+
+            if ((C1.Text == v) && (C2.Text == v) && (C3.Text == "")) return C3;
+            if ((C1.Text == v) && (C2.Text == "") && (C3.Text == v)) return C2;
+            if ((C1.Text == "") && (C2.Text == v) && (C3.Text == v)) return C1;
+
+            //Ver lines
+            if ((A1.Text == v) && (B1.Text == v) && (C1.Text == "")) return C1;
+            if ((A1.Text == v) && (B1.Text == "") && (C1.Text == v)) return B1;
+            if ((A1.Text == "") && (B1.Text == v) && (C1.Text == v)) return A1;
+
+            if ((A2.Text == v) && (B2.Text == v) && (C2.Text == "")) return C2;
+            if ((A2.Text == v) && (B2.Text == "") && (C2.Text == v)) return B2;
+            if ((A2.Text == "") && (B2.Text == v) && (C2.Text == v)) return A2;
+
+            if ((A3.Text == v) && (B3.Text == v) && (C3.Text == "")) return C3;
+            if ((A3.Text == v) && (B3.Text == "") && (C3.Text == v)) return B3;
+            if ((A3.Text == "") && (B3.Text == v) && (C3.Text == v)) return A3;
+
+            //Diag lines
+            if ((A1.Text == v) && (B2.Text == v) && (C3.Text == "")) return C3;
+            if ((A1.Text == v) && (B2.Text == "") && (C3.Text == v)) return B2;
+            if ((A1.Text == "") && (B2.Text == v) && (C3.Text == v)) return A1;
+
+            if ((A3.Text == v) && (B2.Text == v) && (C1.Text == "")) return C1;
+            if ((A3.Text == v) && (B2.Text == "") && (C1.Text == v)) return B2;
+            if ((A3.Text == "") && (B2.Text == v) && (C1.Text == v)) return A3;
+
+            return null;
+
+        }
+
+        private void pvPToolStripMenuItem_Click(object sender, EventArgs e)
         {
             turn = false;
             turnCount = 0;
+            isPvc = false;
+            clearSpaces();
+        }
+
+        private void vsComputerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("PVC");
+            isPvc = true;
+            turn = false;
+            turnCount = 0;
+            clearSpaces();
+        }
+        private void restart()
+        {
+            turn = false;
+            turnCount = 0;
+            clearSpaces();
+        }
+        private void clearSpaces()
+        {
             try
             {
                 foreach (var button in this.Controls.OfType<Button>())
@@ -133,5 +299,7 @@ namespace TicTacToe
             }
             catch { }
         }
+
+
     }
 }
